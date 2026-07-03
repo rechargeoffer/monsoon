c120f80716d746f2a86b7ea76acb4739afb5d76c
rechargeoffer <rajuchacha3753@gmail.com>
2026-07-03 11:34:00 +0000

    Add files via upload

---
+++ static/js/upi.js
@@ -0,0 +1,4 @@
+const upi_id = "yourupi@bankname";
+const upi_name = "Your Name";
+const upi_gateway =
+  "&pn=OfferAvailed&mode=02&mc=8999&purpose=00&cu=INR&sign=AAuN7izDWN5cb8A5scnUiNME+LkZqI2DWgkXlN1McoP6WZABa/KkFTiLvuPRP6/nWK8BPg/rPhb+u4QMrUEX10UsANTDbJaALcSM9b8Wk218X+55T/zOzb7xoiB+BcX8yYuYayELImXJHIgL/c7nkAnHrwUCmbM97nRbCVVRvU0ku3Tr";
